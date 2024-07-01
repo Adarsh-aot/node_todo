8cf6e1a2df4dbf38fd669e71b947cd010c22676a
Adarsh-aot <160816474+Adarsh-aot@users.noreply.github.com>
2024-07-01 04:10:59 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
+// config server
 const express = require('express')
 const todo_route  = require('./todo_route')
 const app = express()
-
 const port = 3000 
 app.use(express.json())
+
+
+
+// routes
 app.use('/todo' , todo_route)
 
 
 
 
-// listen
+// listen server
 app.listen(port , () => {
     console.log("server is running ....")
 })
