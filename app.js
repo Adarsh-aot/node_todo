250a3415bd999e283fa106f31b10f18474965d9a
Adarsh-aot <160816474+Adarsh-aot@users.noreply.github.com>
2024-07-01 02:01:23 -0700

    Assignment

---
+++ app.js
@@ -0,0 +1,22 @@
+
+// config express
+const express = require('express')
+const todo = require('./todo')
+const app = express()
+const port = 3000 
+app.use(express.json())
+
+
+// routes
+app.get('/' , todo.getalldata)
+app.get('/:id' ,  todo.getidbydata)
+app.post('/' , todo.adddata)
+app.delete('/:id' , todo.deletedata)
+app.put('/:id' , todo.putdata)
+
+
+
+// listen
+app.listen(port , () => {
+    console.log("server is running ....")
+})
---
+++ todo.js
@@ -0,0 +1,81 @@
+const data = [
+    {
+        id : 1 ,
+        name : "name"  ,  
+        description : "description"
+    },
+]
+
+
+
+const getalldata = (req , res) => {
+    res.json(data)
+}
+
+const getidbydata =  (req , res) => {
+    const id = req.params.id
+    const todo_id = data.find((todo) => todo.id == id)
+    if(todo_id){
+        return res.json(todo_id)
+    }
+    res.json(
+        {
+            message : "todo not found"
+        }
+    )
+}
+
+
+const adddata =  (req , res) => {
+    const id = req.body.id;
+    const todo = data.find((todo) => todo.id === id)
+    if(!todo){
+        const todo = req.body
+        console.log(todo)
+        data.push(todo)
+        return res.json(data)
+    }
+
+    res.json({                  
+        message : "id already exist"
+    })
+
+    
+
+}
+
+
+const deletedata =  (req , res) => {
+    const id = req.params.id
+    const index = data.findIndex((todo) => todo.id == id)
+    if(index != -1){
+        data.splice(index , 1)
+        return res.json(data)
+    }
+
+    return res.json({
+        message : "todo not found"
+    })
+}
+
+
+
+const putdata =  (req , res) => {
+    const id = req.params.id
+    const index = data.findIndex((todo) => todo.id == id)
+    if(index != -1){
+        data[index] = req.body
+        return res.json(data)   
+    }
+    return res.json({   
+        message : "todo not found"
+    })
+}
+
+module.exports = {
+    getalldata , 
+    getidbydata , 
+    adddata ,
+    deletedata ,
+    putdata
+}
